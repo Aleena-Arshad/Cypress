f7822070c463b5ffaa9dbf500c4969c70a846039
BrainX Technologies <brainxtechnologies3960@BrainXs-MacBook-Air.local>
2025-09-09 15:27:55 +0500

    Initial commit from local folder

--- cypress/e2e/Call_Sling/6.add_audio_file.cy.js
+++ cypress/e2e/Call_Sling/6.add_audio_file.cy.js
@@ -3,10 +3,10 @@ describe('Audio File', () => {
     it('Audio File', () => {
         cy.callsling_login(); // Only login now
         cy.get('.css-u2vdid > .material-icons-outlined').click() //sidebar open
-        // AI drop down open
+        // Audio drop down open
         cy.get('.css-zws9p5 > :nth-child(6) > .MuiBox-root').click()
-        // New AI Agent page
-
+        
+        //
         cy.get("span[class = 'MuiTypography-root MuiTypography-body1 MuiListItemText-primary css-ikhzef']").eq(10).click()
 
         cy.get(':nth-child(2) > a > .MuiListItem-root > .MuiBox-root > .MuiListItemText-root > .MuiTypography-root').eq(1).click()
